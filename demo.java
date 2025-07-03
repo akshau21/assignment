079a0b1711abf29587d7b11515a10096d0118e6e
akashay <wagha9371@gmail.com>
2025-07-03 14:58:23 +0530

    message

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int A=20;
+        int B=45;
+        System.out.println(A+B);
+
+    }
+}
---
+++ noreturn.java
@@ -0,0 +1,11 @@
+// public class noreturn{
+//     public static void main(String args[])
+//     {
+//         int i=100;
+// 		int b=362;
+// 		System.out.println(i+b);
+
+
+//     }
+// }
+
