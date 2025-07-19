f8ad3ee276fc139a5fc81e9d30cc7c4fa8c658a0
Akshay Nagesh wagh <wagha9371@gmail.com>
2025-07-19 20:57:39 +0530

    Add files via upload

---
+++ operater.js
@@ -0,0 +1,44 @@
+
+let a=30              
+
+let x=++a
+console.log("x",x)
+console.log("a",a)
+
+
+let y=a++
+console.log("y",y)
+console.log("a",a)
+
+let z=--a
+console.log("z",z)
+console.log("a",a)
+
+let p=a--
+console.log("p",p)
+console.log("a",a)
+
+let q=++a
+console.log("q",q)
+console.log("a",a)
+
+let s=++a
+console.log("s",s)
+console.log("a",a)
+
+let t=a--
+console.log("t",t)
+console.log("a",a)
+
+let u=++a
+console.log("u",u)
+console.log("a",a)
+
+let v=a--
+console.log("v",v)
+console.log("a",a)
+
+
+
+
+
